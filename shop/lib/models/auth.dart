608a5f57ab23bef43637736da1de03a37eec7ec3
amilton0656 <amilton0656@gmail.com>
2023-08-27 21:42:46 -0300

    home

--- shop/lib/models/auth.dart
+++ shop/lib/models/auth.dart
@@ -9,7 +9,7 @@ class Auth with ChangeNotifier {
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCGHvJvh5GIqIgv1bmkg3cX1o9WDcNKtTQ';
 
   Future<void> _authenticate(String email, String password, String urlFragment) async {
-    const chave = 'AIzaSyCGHvJvh5GIqIgv1bmkg3cX1o9WDcNKtTQ';
+    const chave = 'AIzaSyCGHvJvh5GIqIgv1bmkg3AJRcX1o9WDcNKtTQx';
     final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=$chave';
     print('url>> $url');
     final response = await http.post(
@@ -23,25 +23,11 @@ class Auth with ChangeNotifier {
     print(jsonDecode(response.body));
   }
 
-
   Future<void> signup(String email, String password) async {
     _authenticate(email, password, 'signUp');
-
   }
 
    Future<void> login(String email, String password) async {
     _authenticate(email, password, 'signInWithPassword');
-    // final response = await http.post(
-    
-    //   Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCGHvJvh5GIqIgv1bmkg3cX1o9WDcNKtTQ'),
-                    // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCGHvJvh5GIqIgv1bmkg3cX1o9WDcNKtTQ
-    //   body: jsonEncode({
-    //     'email': email,
-    //     'password': password,
-    //     'returnSecureToken': true,
-    //   }),
-    // );
-    // print(jsonDecode(response.body));
-
   }
 }
